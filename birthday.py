c9669d92b3ccf0675434687191db779c2f286819
sayyyho <323psh@naver.com>
2022-12-28 02:15:56 +0900

    develop

--- birthday.py
+++ birthday.py
@@ -1,14 +1,27 @@
-from random import *
-success = 0
-trial = 100000
-member = 20
+import random
+NUM = 70
 
-for _ in range(trial): #몬테카를로 시뮬레이션 시행
-    check = [0 for i in range (366)]
-    for _ in range(member):
-        random_number = randint(1,365)
-        if(check[random_number]==0):check[random_number] = 1
-        else:
-            success +=1
-            break
-print(success/trial * 100)
+def find_simulation_result():
+    birthday_check = [False for _ in range (366)]
+    for _ in range(NUM):
+        random_birthday = random.randint(1,365)
+        if(birthday_check[random_birthday] == False):birthday_check[random_birthday] = True
+        else:return 1
+    return 0
+
+def find_math_result():
+    complement = 1
+    for i in range(NUM):
+        complement *= (365-i)/365
+    return 1 - complement
+
+def main():
+    trial = 100000
+    match = 0
+    for _ in range(trial):
+        match += find_simulation_result()
+    print("시뮬레이션 결과 : ",format(match/trial * 100, ".4f"),"%",sep="")
+    mathmatical_result = find_math_result()     
+    print("수학적 결과 : ", format(mathmatical_result * 100, ".4f"),"%",sep="")
+
+main()
